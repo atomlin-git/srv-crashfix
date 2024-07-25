b7d2e24c1ff80d55f8f025e2e0ba5112f10a584a
atomlin-git <atomlikn@gmail.com>
2024-07-25 15:53:29 +0300

    migrate to subhook

--- source/main.cpp
+++ source/main.cpp
@@ -1,24 +1,22 @@
 #include "headers.hpp"
 
 urmem::sig_scanner scanner;
-std::pair<urmem::hook, urmem::address_t> hook;
+std::pair<subhook::Hook, urmem::address_t> read_bits_hook;
 
 #ifdef _WIN32
-    bool __fastcall read_bits(void* ptr, void* edx, unsigned char* output, int num_to_read, bool align_to_read) {
+bool __fastcall read_bits(void* ptr, void* edx, unsigned char* output, int num_to_read, bool align_to_read) {
 #elif __linux__
-    bool read_bits(void* ptr, unsigned char* output, int num_to_read, bool align_to_read) {
+bool read_bits(void* ptr, unsigned char* output, int num_to_read, bool align_to_read) {
 #endif
-    int NumberOfUnreadBits =  *((unsigned long *)ptr + 2) > *((unsigned long *)ptr) ? 0 : *((unsigned long *)ptr) - *((unsigned long *)ptr + 2);
-    if(NumberOfUnreadBits < num_to_read) return false;
-    return hook.first.call<urmem::calling_convention::thiscall, bool>(ptr, output, num_to_read, align_to_read);
+    if(UnreadBits(ptr) < num_to_read) return false;
+    auto orig = (r_bits)read_bits_hook.first.GetTrampoline();
+    return !orig ? false : orig(ptr, output, num_to_read, align_to_read);
 };
 
-EXTERN bool CALL Load(void **ppData)
-{ 
+EXTERN bool CALL Load(void **ppData) { 
     if(!scanner.init(reinterpret_cast<urmem::address_t>(ppData[0]))) return false;
-    if(!scanner.find(PATTERN, MASK, hook.second)) return false;
-    hook.first.install(hook.second, urmem::get_func_addr(&read_bits));
-    return true;
-}
+    if(!scanner.find(READ_BITS_PATTERN, READ_BITS_MASK, read_bits_hook.second)) return false;
+    return read_bits_hook.first.Install((void*)(read_bits_hook.second), (void*)read_bits, subhook::HookFlagTrampoline);
+};
 
 EXTERN unsigned int CALL Supports() { return 0x0200 | 0x10000; };
